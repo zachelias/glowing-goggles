f4486493d561af7c86cbc7bac58bb1dd49fd8f95
Zach <eliaszach7@gmail.com>
2016-04-27 21:24:28 +0000

    update w/ bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  resources :quotes
+  root 'static_pages#home'
+  
   get 'static_pages/home'
 
   get 'static_pages/help'
